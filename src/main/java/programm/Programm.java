5ec465b100ac2fca6903afae48726b4160e07473
AltoSax <AltoSax00@bk.ru>
2016-07-28 16:06:32 +0400

    Adding some lines "Hello World"

--- src/main/java/programm/Programm.java
+++ src/main/java/programm/Programm.java
@@ -7,5 +7,18 @@ package programm;
 public class Programm {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
